4ef0790f19697ee93b0955d60aab640ce308c9a3
DanielHenry <daniel.henry.39@gmail.com>
2018-06-02 12:53:34 +0700

    main.cpp di design pattern singleton selesai

--- Singleton/C++/main.cpp
+++ Singleton/C++/main.cpp
@@ -1,5 +1,24 @@
-#include<stdio.h>
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+#include "Logger.h"
 
 int main(){
-    
+    Logger * firstLog;
+    firstLog = Logger::getPointer();
+    firstlog->push("push 1 to firstlog");
+    firstlog->push("push 2 to firstlog");
+
+    Logger * secondlog;
+    secondlog = Logger::getPointer();
+    secondlog->push("push 1 to secondlog");
+    secondlog->push("push 2 to secondlog");
+
+    vector<string> logList = firstlog->getAll();
+    int logListSize = (int)logList.size();
+    for (int i=0;i<logListSize;i++){
+        cout << logList[i] << endl;
+    }
+    return 0;
 }
