b6fc4546778832cb116e3b5914e8caaabbe677c2
DanielHenry <daniel.henry.39@gmail.com>
2018-06-02 13:22:40 +0700

    Logger.cpp belum selesai, masih bingung

---
+++ Singleton/C++/Logger.cpp
@@ -0,0 +1,16 @@
+#include "Logger.h"
+
+Logger::Logger(){
+    //kosong
+    //empty
+}
+static void Logger::getPointer(){
+    static Logger * loggerPtr;
+    
+}
+void Logger::push(string str){
+
+}
+vector<string> Logger::getAll(){
+
+}
